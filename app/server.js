0319479a64d97ec62cd3d39b4334f2b9a85c5da4
Sergio Díaz <codediaz@outlook.es>
2023-11-06 19:19:37 -0500

    init: Local server connect with mongodb

---
+++ app/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/mi_base_datos';
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Conectado a MongoDB');
+}).catch(err => {
+  console.error('Error conectando a MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
